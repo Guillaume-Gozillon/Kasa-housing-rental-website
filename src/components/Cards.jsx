809c51131f45785d3a1a92c6f9a82daf5b2b2e69
Guillaume <guillaumegozillon@gmail.com>
2021-06-24 09:50:55 +0200

    css rental

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Cards = ({item}) => {
     return (
-        <div className="card">
-            <h1>{item.title}</h1>
-            <img src={item.cover} alt="" />
-        </div>
+        <Link to={item.id}>
+            <div className="card">
+                <h1>{item.title}</h1>
+                <img src={item.cover} alt={item.title} />
+            </div>
+        </Link>
     )
 }
 
--- src/components/Rental.jsx
+++ src/components/Rental.jsx
@@ -1,7 +1,9 @@
 import { useHistory } from "react-router-dom"
 import { useState, useEffect } from "react"
 import data from '../data.json' 
+import Header from "./Header"
 
+// Création des CARTE en fonction de leur ID
 const Rental = () => {
     const history = useHistory()
     const url = history.location.pathname
@@ -9,23 +11,36 @@ const Rental = () => {
     const [accomodations, setAccomodations] = useState(data)
     const [accomodation, setAccomodation] = useState(null)
 
-
-    // useEffect
     useEffect(() => {
-        // console.log(`/${stateAccomodation.id}`);
         const currentAccomodation = accomodations
             .filter((stateAccomodation) => `/${stateAccomodation.id}` === url)
             
         setAccomodation(currentAccomodation[0])
     }, [accomodations, url])
 
-    console.log(accomodation.location);
-
     return (
         <>
+            <Header/>
             {accomodation && (
-                <div>
-                    <h1>HEYYY{accomodation.location}</h1>
+                <div className='rental-container'>
+                    <img 
+                        className='rental-image' 
+                        src={accomodation.cover} 
+                        alt={accomodation.title} 
+                    />
+                    <div className='rental-info'>
+                        <div className='rental-left'>
+                            <h1>{accomodation.title}</h1>
+                            <h2>{accomodation.location}</h2>
+                            <ul>
+                                {accomodation.tags.map(item => <li>{item}</li>)}
+                            </ul>
+                        </div>
+                        <div className="rental-right">
+                            <p>{accomodation.host.name}</p>
+                            <img src={accomodation.host.picture} alt="" />
+                        </div>
+                    </div>
                 </div>
             )}
         </>
