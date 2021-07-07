04f925e9770a1fa4cd9a25ec90f59d2366743c26
Guillaume <guillaumegozillon@gmail.com>
2021-07-07 23:40:27 +0200

    Toggle classe

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import React, { Children } from "react";
 
-const Toggle = ({children, title}) => {
-    const [toggle, setToggle] = useState(false)
-    return (
-        <div className='faq active' onClick={() => setToggle(!toggle)}>
-            <div className='faq-title'>
-                <h2>{title}</h2>
-                <i className="fas fa-chevron-down"></i>
+class Toggle extends React.Component {
+    constructor(children) {
+        super(children)
+        this.state = { toggle: false } 
+    }
+
+    handleToggle = () => {
+        const toggled = this.state.toggle === false 
+            ? this.state.toggle = true 
+            : this.state.toggle = false
+
+        this.setState({
+            toggled
+        })
+    }
+
+    render() {
+        return (
+            <div className='faq active' onClick={this.handleToggle}>
+                <div className='faq-title'>
+                    <h2>{this.props.title}</h2>
+                    <i className="fas fa-chevron-down"></i>
+                </div>
+                {this.state.toggle && this.props.children}
             </div>
-            {toggle && children}
-        </div>
-    )
+        )
+    }
 }
 
 export default Toggle
- 
