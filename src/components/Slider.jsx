e5837a37184ebb0eccce24f288e0ae0e6228f551
Guillaume <guillaumegozillon@gmail.com>
2021-07-07 03:30:03 +0200

    Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,81 @@
+import React, { useState } from "react"
+import arrow from '../img/arrow.svg'
+
+class Slider extends React.Component {
+    constructor(accomodation) {
+        super(accomodation)
+
+        this.accomodation = accomodation.accomodation
+        this.state = { image: 0 }
+        this.length = accomodation.accomodation.pictures.length
+    }
+
+    nextSlide = () => {
+        const next = this.state.image === this.length 
+            ? this.state.image = 0 
+            : this.state.image += 1
+
+        this.setState({
+            next
+        })
+    }
+
+    prevSlide = () => {
+        const prev = this.state.image === 0 
+            ? this.state.image = this.length -= 1 
+            : this.state.image -= 1 
+
+        this.setState({
+            prev
+        })
+    }
+
+    testPrev = () => {
+        this.setState(image === 0 
+            ? length - 1 
+            : image - 1)
+    }
+
+    render() {
+        return (
+            <div className='slider'>
+                <img 
+                    src={arrow} 
+                    alt="arrow" 
+                    className='arrow arrow-right' 
+                    onClick={this.nextSlide} 
+                />
+                <img 
+                    src={arrow} 
+                    alt="arrow" 
+                    className=' arrow arrow-left' 
+                    onClick={this.prevSlide} 
+                />
+                {this.accomodation.pictures.map((slide, index) => {
+                    return (
+                        <div 
+                            className={index === this.state.image 
+                            ? 'slide active' 
+                            : 'slide'} key={index}>
+                            {index === this.state.image  && (
+                                <img 
+                                    src={slide} 
+                                    alt={this.accomodation.title}
+                                    className='image-slider' 
+                                />
+                            )}
+                        </div>
+                    )
+                })
+                }
+            </div>
+        )
+    }
+}
+
+export default Slider
+
+{/* 
 import { useState } from "react"
 import arrow from '../img/arrow.svg'
 
@@ -53,3 +131,5 @@ const Slider = ({accomodation}) => {
 }
 
 export default Slider
+
+*/}
